1c2a2367bd08eda2088d362f90bb7c993356c03a
dtwnik <72192760+dtwnik@users.noreply.github.com>
2022-05-22 21:39:59 +0600

    first commit

---
+++ decrypt.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from re import findall
+
+
+def stega_decrypt():
+    a = []
+    keys = []
+    img = Image.open(input("path to image: "))
+    pix = img.load()
+    f = open(input('path to keys: '), 'r')
+    y = str([line.strip() for line in f])
+
+    for i in range(len(findall(r'\((\d+)\,', y))):
+        keys.append((int(findall(r'\((\d+)\,', y)[i]), int(findall(r'\,\s(\d+)\)', y)[i])))
+    for key in keys:
+        a.append(pix[tuple(key)][0])
+    return ''.join([chr(elem) for elem in a])
+
+
+print("you message: ", stega_decrypt())
---
+++ encrypt.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageDraw
+from random import randint
+
+
+def stega_encrypt():
+    keys = []  # сюда будут помещены ключи
+    img = Image.open(input("path to image: "))  # создаём объект изображения
+    draw = ImageDraw.Draw(img)  # объект рисования
+    width = img.size[0]  # ширина
+    height = img.size[1]  # высота
+    pix = img.load()  # все пиксели тут
+    f = open('keys.txt', 'w')  # текстовый файл для ключей
+
+    for elem in ([ord(elem) for elem in input("text here: ")]):
+        key = (randint(1, width - 10), randint(1, height - 10))
+        g, b = pix[key][1:3]
+        draw.point(key, (elem, g, b))
+        f.write(str(key) + '\n')
+
+    print('keys were written to the keys.txt file')
+    img.save("newimage.png", "PNG")
+    f.close()
+
+
+stega_encrypt()
